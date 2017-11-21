65d03b1c0d6619c0effd608006a814700499b510
JCrawford67 <31639002+JCrawford67@users.noreply.github.com>
2017-11-20 22:18:27 -0500

    Initial commit

---
+++ src/main/scala/LanguageMathTranslation.scala
@@ -0,0 +1,9 @@
+/**
+  * Justin Crawford
+  * COSC 455
+  * Project 2
+  */
+
+class LanguageMathTranslation {
+
+}
---
+++ src/main/scala/ScalaWarmup.scala
@@ -0,0 +1,32 @@
+/**
+  * Justin Crawford
+  * COSC 455
+  * Project 2
+  */
+
+class ScalaWarmup {
+
+  // This method takes in an Int and returns T/F based on whether
+  // the parameter passed is a prime number
+  def prime(n: Int): Boolean = {
+
+  }
+
+  // This method takes in 2 Ints and returns T/F based on whether
+  // the parameters passed are twin primes
+  def twinPrimes(n1: Int, n2: Int): Boolean = {
+
+  }
+
+  // This method takes in an Int and returns a list of Ints.
+  // The list is comprised of prime numbers going up to the passed parameter
+  def twinPrimesList(n: Int): List[Int] = {
+
+  }
+
+  // This method takes an Int and prints out a solution that
+  // satisfies Goldbach's conjecture
+  def goldbach (n : Int) = {
+
+  }
+}
